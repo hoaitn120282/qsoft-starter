d1d1e1d86252513e966ca27f9bad6596b630b1c7
HoaiTN <hoai.tn@perseiconsulting.com>
2018-07-25 18:10:34 +0700

    Update app.js

--- app.js
+++ app.js
@@ -50,8 +50,8 @@ const app = express();
 /**
  * Express configuration.
  */
-app.set("host", process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0");
-app.set("port", process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set("host", process.env.NODE_HOST || "0.0.0.0");
+app.set("port", process.env.NODE_PORT || 8080);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(expressStatusMonitor());
