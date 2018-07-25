4ab21a3cb0146351e071f9412849c3cc0ea7ead5
HoaiTN <hoai.tn@perseiconsulting.com>
2018-07-25 10:44:27 +0700

    Baseline version1.0

---
+++ src/migrations/20180725030130-create-user.js
@@ -0,0 +1,60 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },       
+      password: {
+        type: Sequelize.STRING
+      },       
+      passwordResetToken: {
+        type: Sequelize.STRING
+      },       
+      passwordResetExpires: {
+        type: Sequelize.DATE
+      },       
+      facebook: {
+        type: Sequelize.STRING
+      },       
+      twitter: {
+        type: Sequelize.STRING
+      },       
+      google: {
+        type: Sequelize.STRING
+      },       
+      github: {
+        type: Sequelize.STRING
+      },       
+      instagram: {
+        type: Sequelize.STRING
+      },       
+      linkedin: {
+        type: Sequelize.STRING
+      },       
+      steam: {
+        type: Sequelize.STRING
+      },       
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
